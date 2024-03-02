a7a4fcbf3c6b2ef1a96812006893cf709f670309
Michael Campbell <michael.campbell14@gmail.com>
2024-03-02 12:56:55 -0500

    finishing first draft of the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,16 +33,14 @@ const FooterBox = styled.div`
   bottom: 0;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
-  height: 400px;
+  height: 325px;
   background-color: #f27830;
-  /* background-color: #f3e1c2; */
 `;
 
 const BackToTop = styled.div`
   border-radius: 50%;
-  /* border: 1px #de813c solid; */
   border: 1px #f3e1c2 solid;
   width: 100px;
   height: 100px;
@@ -51,15 +49,13 @@ const BackToTop = styled.div`
   align-items: center;
   cursor: pointer;
   transition: background-color 0.5s;
-  margin-bottom: 50px;
 
   &:hover {
     background-color: #f3e1c2;
-    /* background-color: #de813c; */
   }
 
   &:hover .arrow {
-    /* wt pepo colour */
+    /* yt pepo colour */
     /* filter: invert(94%) sepia(6%) saturate(1080%) hue-rotate(345deg) brightness(95%) contrast(99%); */
     /* orange colour */
     filter: invert(82%) sepia(57%) saturate(4284%) hue-rotate(331deg)
@@ -70,7 +66,7 @@ const BackToTop = styled.div`
 const Arrow = styled.img`
   /* orange colour */
   /* filter: invert(82%) sepia(57%) saturate(4284%) hue-rotate(331deg) brightness(90%) contrast(92%); */
-  /* wt pepo colour */
+  /* yt pepo colour */
   filter: invert(99%) sepia(3%) saturate(3199%) hue-rotate(319deg)
     brightness(99%) contrast(93%);
   transition: filter 0.5s;
@@ -81,11 +77,11 @@ const Socials = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 200px;
-  height: 200px;
+  height: 100px;
 `;
 
 const SocialImgs = styled.img`
-  width: 50px;
+  width: 35px;
   /* Dark Brown */
   /* filter: invert(10%) sepia(62%) saturate(965%) hue-rotate(321deg) brightness(102%) contrast(98%); */
   /* Orange */
@@ -94,7 +90,7 @@ const SocialImgs = styled.img`
   /* filter: invert(28%) sepia(22%) saturate(1346%) hue-rotate(346deg) brightness(98%) contrast(85%); */
   /* Green */
   /* filter: invert(39%) sepia(56%) saturate(1707%) hue-rotate(124deg) brightness(97%) contrast(92%); */
-  /* wt pepo colour */
+  /* yt pepo colour */
   filter: invert(99%) sepia(3%) saturate(3199%) hue-rotate(319deg)
     brightness(99%) contrast(93%);
 `;
